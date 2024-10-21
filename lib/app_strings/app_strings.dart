c7aa27baeed5f5718810e396a101dd42d2528e18
Ali Hassan <hassan.jutt646464@gmail.com>
2024-10-22 00:09:12 +0500

    app Strings

--- lib/app_strings/app_strings.dart
+++ lib/app_strings/app_strings.dart
@@ -1,1 +1,19 @@
-
+class AppStrings {
+  static const String creat = 'Create an Account';
+  static const String signUpnow = 'Sign up now to get started with an account';
+  static const String fullName = 'Full Name';
+  static const String emailAddress = 'Email Address';
+  static const String password = 'Password';
+  static const String confirmPassword = 'Confirm Password';
+  static const String iHaveRead = 'I have read and agree to the ';
+  static const String termservice = 'Term of Service';
+  static const String getStarted = 'Get Started';
+  static const String already = 'Already have an account?';
+  static const String login = 'LogIn';
+  static const String donthave = "Don't have an account?";
+  static const String signup = 'SignUp';
+  static const String forget = 'Forget Password?';
+  static const String remember = 'Remember me';
+  static const String welcome = 'Welcome back,please enter your details';
+  static const String logintoyour = 'Login to your Account';
+}
